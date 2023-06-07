ac9e6a8deb7049009000dfbd3fc94c4d93824dfa
Robert Lowe <rlowe@semo.edu>
2023-06-07 19:33:29 -0400

    added initial version of calc before variable binding

---
+++ 07 - Variable Binding/calc/lexer.cpp
@@ -0,0 +1,278 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <cctype>
+#include "lexer.h"
+
+// translate tokens into strings for easy debugging
+const char* TSTR[] = {
+    "INVALID",
+    "EOF",
+    "NEWLINE",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "POW",
+    "LPAREN",
+    "RPAREN",
+    "INTLIT",
+    "REALLIT"
+};
+
+
+//////////////////////////////////////////
+// LexerToken Functions
+//////////////////////////////////////////
+
+// equality operators
+bool LexerToken::operator==(const Token &rhs) const
+{
+    return this->token == rhs;
+}
+
+
+bool LexerToken::operator==(const LexerToken &rhs) const
+{
+    return *this == rhs.token;
+}
+
+
+bool LexerToken::operator!=(const Token &rhs) const
+{
+    return this->token != rhs;
+}
+
+
+bool LexerToken::operator!=(const LexerToken &rhs) const
+{
+    return *this != rhs.token;
+}
+
+
+// print the lexter token (mainly for debugging)
+std::ostream& operator<<(std::ostream &os, const LexerToken &t)
+{
+    return os << TSTR[t.token] << ": \"" << t.lexeme 
+              << "\" Line: " << t.line
+              << " Column: " << t.col;
+}
+
+
+
+//////////////////////////////////////////
+// Lexer Implementation
+//////////////////////////////////////////
+
+// Construct a lexer using std::cin
+Lexer::Lexer() : Lexer(std::cin)
+{
+    // Note the above is how we do constructor delegation!
+    // this space left intentionally blank
+}
+
+
+// Construct a lexer for the given stream
+Lexer::Lexer(std::istream &is) : _is(is)
+{
+    _cur = '\0';        // Start with a null character
+    _line = 1;          // Humans start counting at 1
+    _col = 0;           // We have not scanned a character yet
+    _sigline = false;   // No significant characters found yet
+
+    // start off with an invalid token
+    _curtok.token = INVALID;
+    _curtok.line = _line;
+    _curtok.col = _col;
+}
+
+
+// advance the lexer to the next token
+LexerToken Lexer::next()
+{
+    // get to a significant charcater
+    skip();
+
+    // mark the beginning of the current token (assume it is invalid)
+    _curtok.token = INVALID;
+    _curtok.lexeme = "";
+    _curtok.line = _line;
+    _curtok.col = _col;
+
+    // Try each class of token
+    if(not _is) {
+        _curtok.token = TEOF;
+        return current();
+    } else if(lex_single()) {
+        return current();
+    } else if(lex_number()) {
+        return current();
+    } else {
+        // nothing matched, consume and move on
+        consume();
+        return current();
+    }
+}
+
+
+// get the current token
+LexerToken Lexer::current() const
+{
+    return _curtok;
+}
+
+
+// read the next character from the stream
+void Lexer::read()
+{
+    // handle the end of line
+    if(_cur == '\n') {
+        _line++;
+        _col = 0;
+    }
+
+    // get the character from the current stream
+    _cur = _is.get();
+
+    //update the column information
+    if(_is)
+        _col++;
+}
+
+
+// consume the current character and add it to the lexeme
+void Lexer::consume()
+{
+    // add the current character to the lexeme and get the next one
+    _curtok.lexeme += _cur;
+    read();
+}
+
+// consume all the characters that match the comparison pattern
+void Lexer::consume(std::function<bool(char)> match)
+{
+    while(match(_cur)) {
+        consume();
+    }
+}
+
+
+// skip irrelevant spaces and symbols
+void Lexer::skip()
+{
+    bool in_comment = _cur == '#';
+
+    // read until the next significant character is found
+    while((_is and _cur == '\0') or
+          (not _sigline and _cur =='\n') or
+          (isspace(_cur) and _cur != '\n') or 
+          (in_comment and _cur != '\n') ) 
+    {
+        read();
+        if(_cur == '#') {
+            in_comment = true;
+        } else if(_cur == '\n') {
+            in_comment = false;
+        }
+    }
+
+    // once we are here, we have a significant character
+    // but reset it with newline
+    _sigline = _cur != '\n';
+}
+
+
+// attempt to lex the single character tokens
+bool Lexer::lex_single()
+{
+
+    // assume we have an invalid token
+    _curtok.token = INVALID;
+
+    // match our character
+    switch(_cur) 
+    {
+        case '\n':
+            _curtok.token = NEWLINE;
+            break;
+
+        case '+':
+            _curtok.token = PLUS;
+            break;
+            
+        case '-':
+            _curtok.token = MINUS;
+            break;
+
+        case '*':
+            _curtok.token = TIMES;
+            break;
+
+        case '/':
+            _curtok.token = DIVIDE;
+            break;
+
+        case '^':
+            _curtok.token = POW;
+            break;
+
+        case '(':
+            _curtok.token = LPAREN;
+            break;
+
+        case ')':
+            _curtok.token = RPAREN;
+            break;
+
+        default:
+            return false;
+    }
+
+
+    if(_curtok == INVALID) {
+        // no match was made
+        return false;
+    } else {
+        // consume the match and move on
+        consume();
+        return true;
+    }
+}
+
+
+// attempt to lex a number
+bool Lexer::lex_number()
+{
+    //numbers must begin with a digit
+    if(not isdigit(_cur)) {
+        return false;
+    }
+
+    //we have entered the INTLIT state
+    _curtok.token = INTLIT;
+
+    // consume the digits
+    consume(isdigit);
+
+    //if there is no dot, we are done
+    if(_cur != '.') {
+        return true;
+    }
+
+    //ok, so we have a dot! consume it and transition to invalid
+    consume();
+    _curtok.token = INVALID;
+
+    //Now, if the next symbol is not a number, we have succesfully
+    //matched an invalid token. 
+    if(not isdigit(_cur)) {
+        return true;
+    }
+
+    // it's a real literal! consume the rest and return true
+    _curtok.token = REALLIT;
+    consume(isdigit);
+    return true;
+}
+
+
---
+++ 07 - Variable Binding/calc/op.cpp
@@ -0,0 +1,517 @@
+#include <iostream>
+#include <cmath>
+#include "lexer.h"
+#include "op.h"
+
+//////////////////////////////////////////
+// Helper Functions
+//////////////////////////////////////////
+static ResultType coerce(Result left, Result right) 
+{
+    // if the types match, there is no coercion
+    if(left.type == right.type) return left.type;
+
+    // if either left or right is void, so is the result
+    if(left.type == VOID or right.type == VOID) return VOID;
+
+    // perform type widening
+    if((left.type == REAL and right.type == INTEGER) or 
+       (left.type == INTEGER and right.type == REAL)) {
+        return REAL;
+    }
+
+    //TODO: Technically, if we make it here we have an error. For now, we will
+    //      just default to void. Eventually, we should report a type error.
+    return VOID;
+}
+
+
+//////////////////////////////////////////
+// Multi-Typed Result Returns
+//////////////////////////////////////////
+
+// handy string conversion for debugging
+const char* RTSTR[] = { "VOID", "INTEGER", "REAL" };
+
+// print result values
+std::ostream& operator<<(std::ostream& os, const Result &result)
+{
+    // handle the numeric types
+    if(result.type == INTEGER) return os << result.val.i;
+
+    switch(result.type) {
+        case VOID:
+            break;
+        case INTEGER:
+            os << result.val.i;
+            break;
+        case REAL:
+            os << result.val.r;
+            break;
+    }
+
+    return os;
+}
+
+
+//////////////////////////////////////////
+// UnaryOp Implementation
+//////////////////////////////////////////
+
+// constructor
+UnaryOp::UnaryOp(LexerToken &_token) : ParseTree(_token)
+{
+    this->_child = nullptr;
+}
+
+
+// destructor
+UnaryOp::~UnaryOp() 
+{
+    if(_child) {
+        delete _child;  
+    }
+}
+
+
+// give access to the child
+ParseTree *UnaryOp::child() const
+{
+    return _child;
+}
+
+
+void UnaryOp::child(ParseTree *_child)
+{
+    this->_child = _child;
+}
+
+
+// print the tree with 1 child
+void UnaryOp::print(int depth) const
+{
+    // print ourself
+    ParseTree::print(depth);
+
+    // print our child
+    ParseTree *_child = child();
+    if(_child) {
+        _child->print(depth+1);
+    }
+}
+
+
+//////////////////////////////////////////
+// BinaryOp Implementation
+//////////////////////////////////////////
+
+//constructors
+BinaryOp::BinaryOp(LexerToken &_token) : ParseTree(_token)
+{
+    _lchild = nullptr;
+    _rchild = nullptr;
+}
+
+
+//destructor
+BinaryOp::~BinaryOp()
+{
+    if(_lchild) {
+        delete _lchild;
+    }
+
+    if(_rchild) {
+        delete _rchild;
+    }
+}
+
+
+// give access to the left child
+ParseTree *BinaryOp::left() const
+{
+    return _lchild; 
+}
+
+
+void BinaryOp::left(ParseTree *child)
+{
+    this->_lchild = child; 
+}
+
+
+// give access to the right child
+ParseTree *BinaryOp::right() const
+{
+    return _rchild;
+}
+
+
+void BinaryOp::right(ParseTree *child)
+{
+    this->_rchild = child;
+}
+
+
+// print the tree with 2 children
+void BinaryOp::print(int depth) const
+{
+    ParseTree *l = left();
+    ParseTree *r = right();
+
+    // print the right child
+    if(r) {
+        r->print(depth+1);
+    }
+
+    // print ourself
+    ParseTree::print(depth);
+
+    // print the left child
+    if(l) {
+        l->print(depth+1);
+    }
+}
+
+
+//////////////////////////////////////////
+// NaryOp Implementation
+//////////////////////////////////////////
+
+// construuctor and destructor
+NaryOp::NaryOp(LexerToken _token) : ParseTree(_token)
+{
+    // this space left intentionally blank
+}
+
+
+NaryOp::~NaryOp()
+{
+    // delete all the children
+    for(auto itr = _children.begin(); itr != _children.end(); itr++) {
+        delete *itr;
+    }
+}
+
+
+// push a child onto the list
+void NaryOp::push(ParseTree *child)
+{
+    _children.push_back(child);
+}
+
+
+// access iterators for the children
+std::vector<ParseTree*>::const_iterator NaryOp::begin() const
+{
+    return _children.begin();
+}
+
+
+std::vector<ParseTree*>::const_iterator NaryOp::end() const
+{
+    return _children.end();
+}
+
+
+// print the tree
+void NaryOp::print(int depth) const
+{
+    int n = _children.size();
+    int m = n/2;
+
+    // print the right hand side
+    for(int i=n-1; i>=m; i--) {
+        _children[i]->print(depth+1);
+    }
+
+    //print ourself
+    ParseTree::print(depth);
+
+    // print the left hand side
+    for(int i=m-1; i>=0; i--) {
+        _children[i]->print(depth+1);
+    }
+}
+
+
+//////////////////////////////////////////
+// Program implementation
+//////////////////////////////////////////
+
+Program::Program(LexerToken _token) : NaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+
+Result Program::eval()
+{
+    // evaluate and print each statement in the program
+    for(auto itr = begin(); itr != end(); itr++) {
+        std::cout << (*itr)->eval() << std::endl;
+    }
+
+    // programs return void
+    Result result;
+    result.type = VOID;
+
+    return result;
+}
+
+
+void Program::print(int depth) const
+{
+    int n = _children.size();
+    int m = n/2;
+
+    // print the right hand side
+    for(int i=n-1; i>=m; i--) {
+        _children[i]->print(depth+1);
+    }
+
+    //print ourself
+    print_prefix(depth);
+    std::cout << "PROGRAM" << std::endl;
+
+    // print the left hand side
+    for(int i=m-1; i>=0; i--) {
+        _children[i]->print(depth+1);
+    }
+}
+
+
+//////////////////////////////////////////
+// Add implementation
+//////////////////////////////////////////
+
+Add::Add(LexerToken _token) : BinaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+Result Add::eval() 
+{
+    // evaluate the children
+    Result l = left()->eval();
+    Result r = right()->eval();
+
+    // get the type of the result
+    Result result;
+    result.type = coerce(l, r);
+
+    // perform the operation
+    NUM_ASSIGN(result, NUM_RESULT(l) + NUM_RESULT(r));
+
+    return result;
+}
+
+
+//////////////////////////////////////////
+// Sub implementation
+//////////////////////////////////////////
+
+Sub::Sub(LexerToken _token) : BinaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+Result Sub::eval() 
+{
+    // evaluate the children
+    Result l = left()->eval();
+    Result r = right()->eval();
+
+    // get the type of the result
+    Result result;
+    result.type = coerce(l, r);
+
+    // perform the operation
+    NUM_ASSIGN(result, NUM_RESULT(l) - NUM_RESULT(r));
+
+    return result;
+}
+
+
+//////////////////////////////////////////
+// Mul implementation
+//////////////////////////////////////////
+
+Mul::Mul(LexerToken _token) : BinaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+Result Mul::eval() 
+{
+    // evaluate the children
+    Result l = left()->eval();
+    Result r = right()->eval();
+
+    // get the type of the result
+    Result result;
+    result.type = coerce(l, r);
+
+    // perform the operation
+    NUM_ASSIGN(result, NUM_RESULT(l) * NUM_RESULT(r));
+
+    return result;
+}
+
+
+//////////////////////////////////////////
+// Div implementation
+//////////////////////////////////////////
+
+Div::Div(LexerToken _token) : BinaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+Result Div::eval() 
+{
+    // evaluate the children
+    Result l = left()->eval();
+    Result r = right()->eval();
+
+    // get the type of the result
+    Result result;
+    result.type = coerce(l, r);
+
+    // perform the operation
+    NUM_ASSIGN(result, NUM_RESULT(l) / NUM_RESULT(r));
+
+    return result;
+}
+
+
+//////////////////////////////////////////
+// Pow implementation
+//////////////////////////////////////////
+
+Pow::Pow(LexerToken _token) : BinaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+Result Pow::eval() 
+{
+    // evaluate the children
+    Result l = left()->eval();
+    Result r = right()->eval();
+
+    // get the type of the result
+    Result result;
+    result.type = coerce(l, r);
+
+    // perform the operation
+    NUM_ASSIGN(result, pow(NUM_RESULT(l), NUM_RESULT(r)));
+
+    return result;
+}
+
+
+//////////////////////////////////////////
+// Neg implementation
+//////////////////////////////////////////
+
+Neg::Neg(LexerToken _token) : UnaryOp(_token)
+{
+    // This space left intentionally blank
+}
+
+
+Result Neg::eval()
+{
+    //eval the child and then negate it
+    Result result = child()->eval();
+    NUM_ASSIGN(result, -NUM_RESULT(result));
+
+    return result;
+}
+
+
+void Neg::print(int depth) const
+{
+    print_prefix(depth);
+    std::cout << "NEG: -" << std::endl;
+    child()->print(depth+1);
+}
+
+
+//////////////////////////////////////////
+// Number implementation
+//////////////////////////////////////////
+
+Number::Number(LexerToken _token) : ParseTree(_token)
+{
+    //get the number's value
+    if(_token == INTLIT) {
+        _val.type = INTEGER;
+        _val.val.i = stoi(_token.lexeme);
+    } else if(_token == REALLIT) {
+        _val.type = REAL;
+        _val.val.r = stod(_token.lexeme);
+    }
+}
+
+
+Result Number::eval()
+{
+    return _val;
+}
+
+
+//////////////////////////////////////////
+// ParseTree Implementation
+//////////////////////////////////////////
+
+ParseTree::ParseTree(LexerToken &token)
+{
+    this->_token = token;
+}
+
+
+ParseTree::~ParseTree()
+{
+    //nothing to do
+}
+
+
+// get the token of the parse tree
+LexerToken ParseTree::token() const
+{
+    return _token;
+}
+
+
+// print the tree (for debug purposes)
+void ParseTree::print(int depth) const
+{
+    print_prefix(depth);
+    std::cout << TSTR[token().token] 
+              << ": " << token().lexeme << std::endl;
+}
+
+
+// print the prefix for the tree
+void ParseTree::print_prefix(int depth) const
+{
+    // no prefix for the root.
+    if(depth == 0) return;
+
+    for(int i=1; i<depth; i++) {
+        std::cout << "  |";
+    }
+
+    if(depth > 1) {
+        std::cout << "--+";
+    } else {
+        std::cout << "  +";
+    }
+}
